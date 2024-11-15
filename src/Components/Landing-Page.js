11bb42acae16d97a9e39bf222f15536552fa6565
MJ lynch <mjlynch@MJs-MacBook-Pro.local>
2024-11-15 01:26:11 -0600

    Updating styling to landing page

---
+++ src/Components/Landing-Page.js
@@ -0,0 +1,13 @@
+import react, { useState, useEffect } from 'react';
+
+import "../CSS/landing-page.css";
+
+export default function LandingPage() {
+    return (
+        <div className='landing-page'>
+            <header>
+                <h1>Hello, I'm MJ</h1>
+            </header>
+        </div>
+    );
+}
