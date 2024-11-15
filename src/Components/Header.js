025ac939b1110c9787a4882afd6bf90f5cf40517
MJ lynch <mjlynch@MJs-MacBook-Pro.local>
2024-11-15 02:03:19 -0600

    Added new animations to the styling

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../CSS/header.css";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="header-container">
+                <div className="header-content">
+                    <h1>Hello, I'm MJ</h1>
+                    <h1>Full Stack Developer</h1>
+                    <p>I've been crafting websites for years—mostly because my ideas won't stop bugging me until I do.</p>
+                </div>
+            </div>
+        </header>
+    );
+}
--- src/Components/Landing-Page.js
+++ src/Components/Landing-Page.js
@@ -2,12 +2,41 @@ import react, { useState, useEffect } from 'react';
 
 import "../CSS/landing-page.css";
 
+import Header from './Header';
+
 export default function LandingPage() {
+    const [scroll, setScroll] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScroll(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div className='landing-page'>
-            <header>
-                <h1>Hello, I'm MJ</h1>
-            </header>
+        <div className="landing-page">
+            <div
+                className="landing-main"
+                style={{
+                    transform: `translate3d(0, ${scroll / 100}%, 0) scale(${(100 - scroll / 100) / 100
+                        })`,
+                }}
+            >
+                <h1>SCROLL</h1>
+                <i style={{ fontSize: '50px' }}>☟</i>
+            </div>
+            <div className="landing-page-content">
+                <div className="fade-in-header">
+                    <h1>Hi There!</h1>
+                    <p className="next">Glad you made it!</p>
+                </div>
+            </div>
         </div>
     );
 }
