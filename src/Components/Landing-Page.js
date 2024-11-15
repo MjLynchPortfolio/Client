8418b3d7b230b9795b668068028fe4984e247393
MJ lynch <mjlynch@MJs-MacBook-Pro.local>
2024-11-15 09:33:00 -0600

    Updated the styling and addded images and effects to landing-age

--- src/Components/Landing-Page.js
+++ src/Components/Landing-Page.js
@@ -2,7 +2,8 @@ import react, { useState, useEffect } from 'react';
 
 import "../CSS/landing-page.css";
 
-import Header from './Header';
+import iphoneDemo from '../Images/Iphone-demo.png';
+
 
 export default function LandingPage() {
     const [scroll, setScroll] = useState(0);
@@ -21,20 +22,39 @@ export default function LandingPage() {
 
     return (
         <div className="landing-page">
+            <div className='header-gradient'></div>
             <div
                 className="landing-main"
                 style={{
                     transform: `translate3d(0, ${scroll / 100}%, 0) scale(${(100 - scroll / 100) / 100
                         })`,
+
                 }}
             >
-                <h1>SCROLL</h1>
-                <i style={{ fontSize: '50px' }}>☟</i>
+                <div className="fade-in-header">
+                    <div className='header-content'>
+                        <div className='header-image'>
+                            <img src={iphoneDemo} alt='Iphone Demo' />
+                        </div>
+                        <div className='header-text'>
+                            <h1>Hi, I'm MJ</h1>
+                            <h1>Full Stack Developer</h1>
+                            <p className='next'>I've been crafting websites for years—mostly because my ideas won't stop bugging me until I do.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='scroll'>
+                    <h1>SCROLL</h1>
+                    <i style={{ fontSize: '20px' }}>☟</i>
+                </div>
             </div>
             <div className="landing-page-content">
-                <div className="fade-in-header">
-                    <h1>Hi There!</h1>
-                    <p className="next">Glad you made it!</p>
+                <div className='content-container'>
+                    <div className='content'>
+                        <h1>My Projects</h1>
+                        <p>Behold! A collection of my finest (and maybe a few questionable) creations. Click on the images to dive into the sites that kept me up at night.</p>
+
+                    </div>
                 </div>
             </div>
         </div>
