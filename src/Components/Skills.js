c17ddcbea80e184deea0a87144b0fb8e2b051ac9
MJ lynch <mjlynch35@yahoo.com>
2024-11-16 11:57:06 -0600

    updateded spelling

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -10,7 +10,7 @@ export default function Skills() {
                     <h1>HTML | CSS | JS | REACT | NODE.JS | MYSQL  </h1>
                 </div>
                 <div className="triple-bar">
-                    <div className="bar">2.5+<br /> years</div>
+                    <div className="bar">2.5+<br /> Yrs</div>
                     <div className="bar">50+<br /> Website</div>
                     <div className="bar">20k+<br /> Hours</div>
                 </div>
