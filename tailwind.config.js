36e8d93217e11aac293795e599c1328ae91e8f12
MJ Lynch <mjlynch35@yahoo.com>
2025-03-11 20:13:44 -0500

    Added tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        gradient: "gradient-animation 6s linear infinite",
+      },
+      keyframes: {
+        "gradient-animation": {
+          "0%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" },
+          "100%": { "background-position": "0% 50%" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
