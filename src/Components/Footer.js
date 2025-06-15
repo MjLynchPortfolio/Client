8862318a52c69d14cfe96cee8eca6ba96d883213
MJ Lynch <mjlynch35@yahoo.com>
2025-06-15 00:09:37 -0500

    No commit message provided or no significant changes made.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -63,7 +63,7 @@ export default function Footer() {
                     </div>
 
                     {/* Resume Download Button */}
-                    <motion.a
+                    {/* <motion.a
                         href="/MartinLynchResumeMain.pdf"// <-- Path to your resume file
                         download="Martin_Lynch_Resume.pdf"
                         whileHover={{ scale: 1.05 }}
@@ -71,7 +71,7 @@ export default function Footer() {
                         className="bg-blue-500 hover:bg-blue-600 text-white px-8 py-3 rounded-full font-semibold transition duration-300"
                     >
                         Download Resume
-                    </motion.a>
+                    </motion.a> */}
                 </motion.div>
             </div>
 
